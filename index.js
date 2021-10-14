569035b23fbfd61c40401f93c38c8ac3d1b1dc9f
Alexandra <alexandralejarza@gmail.com>
2021-10-14 18:34:13 -0400

    Create array and variables for wins and losses

---
+++ index.js
@@ -0,0 +1,31 @@
+// Create an Array of at least 3 losing messages
+var messages = [
+  "You lost! Better luck next time.", 
+  "Don't quit your day job!", 
+  "You are not good at this."
+]
+// Create variables to count wins and losses
+var wins = 0
+var losses = 0
+// Create variables that target elements with the following IDs: 'message', 'wins', 'losses'
+
+// target all .box elements and attach a click event listener to each one using a loop
+
+// within each click event...
+// determine which box was clicked with 'this.textContent' or event.target.textContent
+// convert that value to a Number and store it to a variable
+
+// create a random number between 1-3 and store it to a variable
+// This number will represent the winning box
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+
+// determine if the box clicked is equal to the random number
+// if the numbers match, display a winning message by changing the text content of the div#message element
+// if the numbers match, increment wins and display the win count in div#wins
+
+// if the numbers don't match, change the div#message element's text to a random losing message from the array above
+// if the numbers don't match, increment losses and display the loss count in div#losses
